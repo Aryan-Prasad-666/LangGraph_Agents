3be11cc6b8fa56f68ba92383f850d9f96465eb13
Aryan-Prasad-666 <aryanprasad6aug@gmail.com>
2025-07-02 18:10:26 +0530

    langgraph tool

---
+++ chatbot2.py
@@ -0,0 +1,65 @@
+from typing import Annotated
+import os
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_groq import ChatGroq
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+
+load_dotenv()
+
+groq_key = os.getenv('groq_api_key')
+
+llm = ChatGroq(
+    model = "deepseek-r1-distill-llama-70b",
+    api_key= groq_key,
+    temperature= 0.6
+)
+
+tool = TavilySearch(max_results = 2)
+tools = [tool]
+
+llm_with_tool = llm.bind_tools(tools)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+def tool_calling_llm(state: State):
+    return {"messages": [llm_with_tool.invoke(state["messages"])]}
+
+
+graph_builder.add_node("tool_calling_llm", tool_calling_llm)
+graph_builder.add_node("tools", ToolNode(tools))
+
+graph_builder.add_edge(START, "tool_calling_llm")
+graph_builder.add_conditional_edges(
+    "tool_calling_llm",
+    tools_condition
+)
+graph_builder.add_edge("tools", END)
+graph = graph_builder.compile()
+
+from IPython.display import Image, display
+
+try:
+    display(Image(graph.get_graph().draw_mermaid_png()))
+except Exception:
+    pass
+
+response = graph.invoke({"messages":"tell me the recent ai news"})
+
+print(response['messages'][-1].content)
+
+for m in response['messages']:
+    m.pretty_print()
+
